b64caf7043fc391b10195745a711e1aa5eacf597
hriships <hrishishinde.p@gmail.com>
2016-11-22 04:57:37 +0530

    Added new Db operations and subscribe event processor

--- src/main/java/com/appdirect/subscriptions/notifications/DBRepository.java
+++ src/main/java/com/appdirect/subscriptions/notifications/DBRepository.java
@@ -3,13 +3,21 @@ package com.appdirect.subscriptions.notifications;
 import com.appdirect.subscriptions.notifications.domain.NotificationType;
 import com.appdirect.subscriptions.notifications.domain.SubscriptionNotification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by hrishikeshshinde on 21/11/16.
  */
 
-@Repository
+@RepositoryRestResource
 public interface DBRepository extends JpaRepository<SubscriptionNotification, Long> {
-    SubscriptionNotification getNotificationByType(NotificationType type);
+
+    @Query("FROM SubscriptionNotification s where s.type = :type")
+    Collection<SubscriptionNotification> findBySubscriptionNotificationType(@Param("type") NotificationType type);
 }
---
+++ src/main/java/com/appdirect/subscriptions/operations/CreateEventProcess.java
@@ -0,0 +1,11 @@
+package com.appdirect.subscriptions.operations;
+
+/**
+ * Created by hrishikeshshinde on 22/11/16.
+ */
+public class CreateEventProcess implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
